a995fe71a16889184de6fdc49951f2a5f5d2a66a
JasonArriaza <JasonArriaza10@gmail.com>
2024-01-27 20:03:05 -0600

    added woriking questions in terminal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -7,9 +8,51 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
     //objects of questions
     {
-
-    },
-
+        type: 'input',
+        name: 'title',
+        message: 'Project Name?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of the project?',
+      },
+      {
+        type: 'input',
+        name: 'tableContents',
+        message: 'What are your table of contents?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What is your installation process?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How do we use this application',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license from list below that you are using',
+        choices: ['MIT', 'Microsoft Public License', 'Mozilla Public License 2.0'],
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'How do we input into this application',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'How can we test this product?',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'Any questions?',
+      },
 
 ];
 
@@ -20,17 +63,6 @@ function writeToFile(fileName, data) { }
 function init() {
     inquirer.prompt(questions)
         .then((answers) => {
-            // const mockAnswers = {
-            // title: 'Project title',
-            // description: 'project description',
-            // tableOfcontents: 'the table of contents',
-            // installation: 'project install',
-            // usage: 'project usage',
-            // license: 'license',
-            // contributing: 'contributing',
-            // tests: 'test',
-            // questions: 'question'
-            // };
             const readme = generateMarkdown(answers);
             console.log(readme);
         })
