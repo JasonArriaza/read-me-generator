ca90f6c333ff17c0585a4351697465b565cef928
Jason <JasonArriaza10@gnail.com>
2024-01-27 19:24:04 -0600

    added the new set up

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
+    //objects of questions
+    {
+
+    },
 
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 function init() {
-    const mockAnswers = {
-        title: 'Project title',
-        description: 'project description',
-        tableOfcontents: 'the table of contents',
-        installation: 'project install',
-        usage: 'project usage',
-        license: 'license',
-        contributing: 'contributing',
-        tests: 'test',
-        questions: 'question'
-    };
-    const readme = generateMarkdown(mockAnswers);
-    console.log(readme);
+    inquirer.prompt(questions)
+        .then((answers) => {
+            // const mockAnswers = {
+            // title: 'Project title',
+            // description: 'project description',
+            // tableOfcontents: 'the table of contents',
+            // installation: 'project install',
+            // usage: 'project usage',
+            // license: 'license',
+            // contributing: 'contributing',
+            // tests: 'test',
+            // questions: 'question'
+            // };
+            const readme = generateMarkdown(answers);
+            console.log(readme);
+        })
 }
-
 // Function call to initialize app
 init();
